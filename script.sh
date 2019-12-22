3a82086d846dbf9ded8ad928a1777a878a2773ca
Alexander Jr <aafonso.93.dev@gmail.com>
2019-12-21 22:58:56 -0300

    first commit

---
+++ script.sh
@@ -0,0 +1,21 @@
+!/bin/sh
+# This is a comment!
+#apt-get update
+apt-get install git
+apt-get --yes install meld
+#apt-get remove docker docker-engine docker.io
+apt-get --yes install docker.io
+apt-get --yes install docker-compose
+apt-get --yes install apt-transport-https
+apt-get --yes install code
+apt-get --yes install dbeaver-ce
+apt-get --yes install deepin-terminal
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt-get --yes install docker-ce
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /us$
+sudo chmod +x /usr/local/bin/docker-compose
+ssh-keygen
